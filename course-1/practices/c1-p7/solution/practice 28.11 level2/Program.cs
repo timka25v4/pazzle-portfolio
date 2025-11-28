4306a4fd5a34512721bd361f340f86f61db3fa72
TimKa25v4 <timka25v4@gmail.com>
2025-11-28 22:14:06 +0500

    feat: добавлена практика за 28.11

---
+++ course-1/practices/c1-p7/solution/practice 28.11 level2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.ComponentModel;
+
+class Program
+{
+	static void Main()
+	{   //Уровень 2
+		//Задание 5: Калькулятор оценок
+		int grade = int.Parse(Console.ReadLine());
+		if (grade == 3)
+		{
+			Console.WriteLine("Удовлетворительно");
+		}
+		else if (grade == 4)
+		{
+			Console.WriteLine("Хорошо");
+		}
+		else if (grade == 5)
+		{
+			Console.WriteLine("Отлично");
+		}
+		//Задание 6: Калькулятор
+		double a = double.Parse(Console.ReadLine());
+		double b = double.Parse(Console.ReadLine());
+		string command = Console.ReadLine();
+		if (command == "+")
+		{
+			Console.WriteLine(a + b);
+		}
+		else if (command == "-")
+		{
+			Console.WriteLine(a - b);
+		}
+		else if (command == "*")
+		{
+			Console.WriteLine(a * b);
+		}
+		else if (command == "/")
+		{
+			Console.WriteLine(a / b);
+		}
+		//Задание 7: Проверка пароля
+		string password = 'drowssap';
+		string user_pass = Console.ReadLine();
+		if (password == user_pass)
+		{
+			Console.WriteLine("Доступ получен");
+		}
+		else
+		{
+			Console.WriteLine("Доступ запрещён");
+		}
+	}
+}
---
+++ course-1/practices/c1-p7/solution/practice 28.11/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Numerics;
+
+class Program
+{
+	static void Main()
+	{   //Уровень 1
+		//Задание 1: Проверка числа
+		int number = 10;
+		if (number > 0)
+		{
+			Console.WriteLine("Число положительное");
+		}
+		else if (number < 0)
+		{
+			Console.WriteLine("Число отрицательное");
+		}
+		else
+		{
+			Console.WriteLine("Число равно нулю");
+		}
+		//Задание 2: Проверка возраста
+		int age = int.Parse(Console.ReadLine());
+		if (age >= 18)
+		{
+			Console.WriteLine("Вы совершеннолетний");
+		}
+		else
+		{
+			Console.WriteLine("Вы несовершеннолетний");
+		}
+		//Задание 3: Проверка чётности числа
+		int num = 7;
+		if (num % 2 == 0)
+		{
+			Console.WriteLine("Число чётное");
+		}
+		else
+		{
+			Console.WriteLine("Число нечётное");
+		}
+		//Уровень 2
+		//Задание 4: Логические операторы
+		int a = 5, b = -2;
+		if (a > 0 && b > 0)
+		{
+			Console.WriteLine("Оба числа положительные");
+		}
+		else if (a > 0 || b > 0)
+		{
+			Console.WriteLine("Хотя бы одно число положительное");
+		}
+		if (a < 0)
+		{
+			Console.WriteLine("a не положительное");
+		}
+	}
+}
